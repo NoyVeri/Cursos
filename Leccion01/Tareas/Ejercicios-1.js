1fb9b8ad2a3b9d6c3aba92c26a067076ffd5ac09
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-06-14 03:58:58 -0600

    Updated

---
+++ Leccion01/Tareas/Ejercicios-1.js
@@ -0,0 +1,12 @@
+var n1 = 12;
+var n2 = 2;
+var suma;
+var resta;
+var multi;
+
+
+suma = n1 + n2;
+resta = n1 - n2;
+multi = n1 * n2;
+
+console.log("suma = ",suma,"resta =  ",resta,"multiplicacion = ",multi);
