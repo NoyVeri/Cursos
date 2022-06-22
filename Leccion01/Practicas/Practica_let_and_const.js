9afe0852dbd8621c1ba69ef6cb5fd1ce1b851185
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-06-22 17:42:15 -0600

    Updated

---
+++ Leccion01/Practicas/Practica_let_and_const.js
@@ -0,0 +1,27 @@
+// en esta practica se lleva a acabo las pruebas que surgan con el let y el const
+
+/* ejercicio de ejemplo: hacer que si te dan permiso a salir imprima un siuu, de lo contrario
+mandar a imprimir un chale que mala onda 
+*/
+
+let jefa = 'si';
+const jefe = 'si';
+
+if(jefa == 'si'){
+console.log("preguntale a tu padre");
+console.log("*le pregunta xd*");
+
+if(jefe == 'si'){
+
+    console.log("siuuuuuuuu");
+}else if (jefe != 'si'){
+
+    console.log("chale que mala onda jefe");
+}
+
+}else if (jefa != 'si'){
+
+console.log("chale que mala onda jefa");
+console.log("*se hace mariguano*");
+
+}
