4e3efe8160d3b875efc23bac2bd2cc87ddd86e1e
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-06-29 14:41:06 -0600

    Updated

---
+++ Leccion01/Practicas/practica-objetos.js
@@ -0,0 +1,21 @@
+
+let persona = {
+datosPersona1:{
+nombreCompleto: 'Juan Adame',
+apellidoCompleto: 'Pepe Irino',
+edad: 10,
+sexo: 'masculino',
+estudia: 'si'
+},
+datosPersona2:{
+    nombreCompleto: 'Alama',
+    apellidoCompleto: 'Marcela Goso',
+    edad: 14,
+    sexo: 'femenino',
+    estudia: 'si'
+}
+    
+};
+
+
+
