c951638846d11c744c0ac30b9edeeb4aaf125bb1
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-06-30 12:55:03 -0600

    Updated

--- Leccion01/Practicas/practica_operadoresAritmeticos.js
+++ Leccion01/Practicas/practica_operadoresAritmeticos.js
@@ -8,16 +8,16 @@ let divicion;
 let suma;
 let union;
 
-union = b**2 - 4*a*c;
-raiz = Math.sqrt(union);
-divicion = 2*a
-suma = -b + raiz
+union = b ** 2 - 4 * a * c;
+raiz = Math.sqrt( union );
+divicion = 2 * a;
+suma = -b + raiz;
 xPositivo = suma / divicion;
-console.log(xPositivo);
+console.log( "Valor de X1 :" + xPositivo );
 
-union = b**2 - 4*a*c;
-raiz = Math.sqrt(union);
-divicion = 2*a
-suma = -b - raiz
+union = b ** 2 - 4 * a * c;
+raiz = Math.sqrt( union );
+divicion = 2 * a;
+suma = -b - raiz;
 xNegativo = suma / divicion;
-console.log(xNegativo);
+console.log( "Valor de X2 :" + xNegativo );
