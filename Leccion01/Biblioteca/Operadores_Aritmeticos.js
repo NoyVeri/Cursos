57be618d97751181ca9986de1965132879a93040
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-06-30 12:30:02 -0600

    Updated

--- Leccion01/Biblioteca/Operadores_Aritmeticos.js
+++ Leccion01/Biblioteca/Operadores_Aritmeticos.js
@@ -5,5 +5,9 @@ let a = 3, b = 2;
 let z = a + b;
 console.log( z );
 // operador de la resta - 
-z = a -b;
+z = a - b;
 console.log( z );
+// operador de la multiplicacion *
+z = a * b;
+console.log( z );
+//operador de la divicion /
