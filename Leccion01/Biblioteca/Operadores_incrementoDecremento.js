1440d61bddb2354146b28241f420a3b8ec5cf359
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-07-01 16:24:02 -0600

    Updated

---
+++ Leccion01/Biblioteca/Operadores_incrementoDecremento.js
@@ -0,0 +1,9 @@
+let a = 3, b = 2;
+let z;
+// Incremento
+/*Pre-incremento (el operador ++ antes de la variable) es cuando incrementamos la variable antes de 
+asignarla a otra variable
+*/
+
+z = ++a;
+console.log(z);
