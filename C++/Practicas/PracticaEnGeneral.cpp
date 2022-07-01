e01c79ccdb601bca471a44987f947ba5a1c9cd7c
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-07-01 17:12:32 -0600

    Updated

---
+++ C++/Practicas/PracticaEnGeneral.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+	int num[100][100],filas,colu;
+	
+	cout<<"numero de filas: "<<endl; cin>>filas;
+	cout<< "numero de columnas: "<<endl; cin>>colu;
+	
+	
+	for(int i=0;i<filas;i++){
+		for(int j=0;j<colu;j++){
+			cout<<"Escribe los numero de orden de izquierda a derecha ["<<i<<"]["<<j<<"]: ";
+			cin>>num[i][j];
+		}
+	} 
+	
+	cout<<"\nMatriz final\n\n";
+	
+	for(int i=0;i<filas;i++){
+		for(int j=0;j<colu;j++){
+			cout<<"[";
+			cout<<num[i][j];
+			cout<<"]";
+			
+		}
+		
+		cout<<"\n";
+	}
+	
+	
+	getch();
+	return 0;
+}
