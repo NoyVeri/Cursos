e9a7bf43cdc9b1fdd048ee6ad77ff1088a276cc9
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-07-05 13:38:25 -0600

    Updated

--- JavaScript/Biblioteca/Precedencia-operadores.js
+++ JavaScript/Biblioteca/Precedencia-operadores.js
@@ -1,6 +1,10 @@
 /*
 Cada operador tiene un rango de prioridad, el cual dependiendo de este se realizaria primero
-o despues
+o despues de otro operador.
+
+cuando los dos operadores tienen el mismo rango de prioridad, se hara el pimero que se encuentre de
+izquierda a derecha es decir : 2*2*3, primero se hara el 2 * 2 cuyo resultado sera multiplicado por 3
+lo cual nos queda el resultado de 12.
 
 */
 
@@ -15,4 +19,13 @@ console.log( z );
 z = a * ( b + c );
 console.log( z );
 
-//
+// Segundo rango se encuentran los incrementos o decrementos
+
+z = a + --b;
+console.log ( z );
+
+// Tercer rango se encuentran los operadores de multipliacion, divicion y modulo
+b = ++b;
+ 
+z = a + b * c;
+console.log( z );
