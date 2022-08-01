b9982584b1dd059827b646213fcf151f5d6a1e96
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-07-31 20:10:04 -0600

    Updated

---
+++ JavaScript/Practicas/Método-Newton-Raphson.js
@@ -0,0 +1,16 @@
+let Xi = 0, formu;
+const e = 2.718281828;
+
+
+for (i = 0; i <= 4; i++){
+    X = i;
+    p1 = ( e**-Xi )-Xi;
+    p2 = -(e**-Xi)-1;
+    p1 = p1 / p2;
+    formu = Xi - p1;
+
+    Et = ((formu - Xi) / formu)*100;
+    Xi = formu;
+    console.log( "Resultado de la raíz",formu,"porcentaje de errro", Et,"%" );
+    
+}
