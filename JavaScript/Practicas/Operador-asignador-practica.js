d191d0541300931a052b42f3ddfc62b42dd01e5e
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-09-26 14:37:56 -0600

    Updated

---
+++ JavaScript/Practicas/Operador-asignador-practica.js
@@ -0,0 +1,16 @@
+// pruebas sin sentido
+
+let a = 2,b = 1,c = 3,d = 6,e = 3;
+
+a += 12;
+b *= a;
+c **= b;
+d -= c;
+e /= d;
+
+
+console.log( a );
+console.log( b );
+console.log( c );
+console.log( d );
+console.log( e );
