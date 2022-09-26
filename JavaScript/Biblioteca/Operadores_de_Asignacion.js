0ffc11e527ffe4630b1eec060b730df9428786fa
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-09-26 14:07:08 -0600

    Updated

---
+++ JavaScript/Biblioteca/Operadores_de_Asignacion.js
@@ -0,0 +1,11 @@
+/*
+Los operadores de asignacion son aquellos que usamos para dar un valor a una variable, el más comun es "="
+el cual hace que cualquier valor que pongamos despues del "=" se almacene en la variable
+*+/-**%
+*/
+// Primer tipo "=" sirve para almacenar cualquier valor en una variable
+let a = 12;
+console.log( a );
+/*
+
+*/
