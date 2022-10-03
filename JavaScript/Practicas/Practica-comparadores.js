a0656e05c14de982bfcdfd20e016130da166d8a0
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-10-03 15:38:29 -0600

    Update

---
+++ JavaScript/Practicas/Practica-comparadores.js
@@ -0,0 +1,24 @@
+// contador de etiquetas
+
+let a = 1, d = "1";
+
+
+
+switch (a == d){
+
+    case a === d:
+console.log("que rico la mamas chvo");
+ break;
+ case a !== d:
+    console.log("a mamonsito el pibe");
+    a += 1;
+    break;
+
+}
+
+
+
+
+
+
+
