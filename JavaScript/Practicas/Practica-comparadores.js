56dcc38e281a02abb0aea04822fda2d07c0403d8
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-10-03 15:47:10 -0600

    Updated

--- JavaScript/Practicas/Practica-comparadores.js
+++ JavaScript/Practicas/Practica-comparadores.js
@@ -2,25 +2,27 @@
 
 let a = 1, d = "1", r = 1;
 
-switch (a == d){
+switch ( a == d ){
 
-    case a === d:
-console.log("d es un valor entero");
+ case a === d:
+console.log( "d es un valor entero" );
  break;
+
  case a !== d:
-    console.log("Es un char el valor de d cuyo valor es", d);
-    break;
+    console.log( "Es un char el valor de d cuyo valor es", d );
+ break;
 
 }
 
-switch (a == r){
+switch ( a == r ){
 
-    case a === r:
-console.log("r es un valor entero");
+ case a === r:
+console.log( "r es un valor entero" );
  break;
+
  case a !== r:
-    console.log("Es un char el valor de d cuyo valor es", r);
-    break;
+    console.log( "Es un char el valor de d cuyo valor es", r );
+ break;
 
 }
 
