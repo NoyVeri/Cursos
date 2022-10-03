dc7caabfd8527447844caf5ef97c24a71d1ddd39
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-10-03 15:45:12 -0600

    Updated

--- JavaScript/Practicas/Practica-comparadores.js
+++ JavaScript/Practicas/Practica-comparadores.js
@@ -1,23 +1,28 @@
 // identificador de etiquetas
 
-let a = 1, d = "1";
-
-
+let a = 1, d = "1", r = 1;
 
 switch (a == d){
 
     case a === d:
-console.log("que rico la mamas chvo");
+console.log("d es un valor entero");
  break;
  case a !== d:
-    a += 1;
     console.log("Es un char el valor de d cuyo valor es", d);
-    console.log( a );
     break;
 
 }
 
+switch (a == r){
 
+    case a === r:
+console.log("r es un valor entero");
+ break;
+ case a !== r:
+    console.log("Es un char el valor de d cuyo valor es", r);
+    break;
+
+}
 
 
 
