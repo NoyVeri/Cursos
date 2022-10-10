652d4d1d160f94a196ea69e4bc3c35ba666da8db
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-10-10 14:19:41 -0600

    Updated

---
+++ JavaScript/Biblioteca/Operadores_Relacional.js
@@ -0,0 +1,18 @@
+let a = 2; b = 1;
+
+//Menor que 
+c = a < b;
+console.log( c );
+
+// Menor o igual a
+c = a <= b;
+console.log( c );
+
+// Mayor que
+c = a > b;
+console.log( c );
+
+// Mayor o igual que
+c = a >= b;
+console.log( c );
+
