d66f08a7fe27dcc7dbf41a570318e2548759e14c
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-10-17 19:08:12 -0600

    Updated

---
+++ JavaScript/Practicas/esMayordeEdad.js
@@ -0,0 +1,14 @@
+//Programa para saber si una persona es mayor de edad
+let persona = 18, Adulto = 18, minimoParaserAdulto = 17.60;
+
+
+if( persona >= Adulto ){
+
+    console.log( "La persona es mayor de edad" );
+}else if( persona >= minimoParaserAdulto ){
+
+    console.log( "Esta persona se le puede considerar como adulto" );
+}else{
+
+    console.log( "Esta persona es menor de edad" );
+}
