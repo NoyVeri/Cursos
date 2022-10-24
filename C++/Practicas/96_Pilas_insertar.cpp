922657cd592c9ce8b99968dfb06185716ea8297d
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2022-10-23 20:21:21 -0600

    Updated

---
+++ C++/Practicas/96_Pilas_insertar.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+struct Nodo
+{
+    int dato;
+    Nodo *siguiente;
+};
+
+
+void agregarPila(Nodo *&,int);
+
+int main(){
+Nodo *pila = NULL;
+int n1,n2;
+cout<<"Digite un numero: ";
+cin>>n1;
+agregarPila(pila,n1);
+cout<<"Digite un numero: ";
+cin>>n2;
+agregarPila(pila,n2);
+
+getch();
+    return 0;
+}
+void agregarPila(Nodo *&pila, int n){
+    Nodo *nuevo_nodo = new Nodo();
+    nuevo_nodo->dato = n;
+    nuevo_nodo->siguiente = pila;
+    pila = nuevo_nodo;
+
+    cout<<"\nElemento "<<n<<" agregado a Pila correctamente"<<endl;
+}
+
+
