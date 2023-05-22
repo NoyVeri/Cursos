b799047f12b86702f635276c0b7fbcee099bac92
NoyVeri <manuelignaciodominguezlozano@gmail.com>
2023-05-22 15:18:38 -0600

    CONFIRT

---
+++ JavaScript/Practicas/Practica1.js
@@ -0,0 +1,29 @@
+const lope = 3;
+let de = 34;
+
+de +=6 
+
+console.log(de, lope);
+
+let pepe;
+
+pepe = 3;
+
+
+for(i = 0; i <= pepe;i++){
+    console.log("hola que hace");
+}
+
+let letra = '2', numero = 2;
+ayuda = 2;
+if(letra === ayuda){
+    console.log("ira wey soy un dos pero en nuemro")
+}else{
+    console.log("chale wey soy una variable tipo char pipipipi")
+}
+if(numero === ayuda){
+    console.log("ira wey soy un numero real")
+}
+amor = 2;
+do{console.log("te amo bb");}
+while(amor != 1);
